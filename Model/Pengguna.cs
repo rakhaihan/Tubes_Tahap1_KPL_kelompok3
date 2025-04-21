1507561b7b2a4350f336d9fee8078b84c080a46d
Ra.khaihan <rakharaihan2244@gmail.com>
2025-04-21 09:26:21 +0700

    menambahkan Model

---
+++ Model/Pengguna.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_Tahap1_KPL_kelompok3.Model
+{
+    public class Pengguna
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public RolePengguna Role { get; set; }
+    }
+}
