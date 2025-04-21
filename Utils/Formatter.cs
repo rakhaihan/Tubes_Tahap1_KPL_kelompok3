681a41b9c70ff4a45c3fd96ba83ddf1de5842a9c
Ra.khaihan <rakharaihan2244@gmail.com>
2025-04-21 10:12:37 +0700

    update Utils

---
+++ Utils/Formatter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_Tahap1_KPL_kelompok3.Utils
+{
+    internal class Formatter
+    {
+        public static string FormatTanggal(DateTime tanggal) => tanggal.ToString("dd MMM yyyy");
+    }
+}
