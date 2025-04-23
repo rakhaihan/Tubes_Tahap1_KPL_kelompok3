09915d831bbdd6788a233b28a12ec9827b92dd52
azzkiabdl <147375620+azzkiabdl@users.noreply.github.com>
2025-04-23 19:53:48 +0700

    Menambahkan Parameterization/generics

---
+++ Components/TableRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_Tahap1_KPL_kelompok3.Components
+{
+    public static class TableRenderer
+    {
+        public static void Render<T>(List<T> data, Dictionary<string, Func<T, string>> columns)
+        {
+            Console.WriteLine(string.Join(" | ", columns.Keys));
+
+            foreach (var item in data)
+            {
+                var row = columns.Values.Select(selector => selector(item));
+                Console.WriteLine(string.Join(" | ", row));
+            }
+        }
+    }
+}
+
