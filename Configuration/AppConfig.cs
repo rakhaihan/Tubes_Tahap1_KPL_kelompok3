c13691edcbd6ba065955e31245c4372be5c8ae10
Zipooxy <142865402+Zipooxy@users.noreply.github.com>
2025-04-23 19:33:08 +0700

    menambahkan config

---
+++ Configuration/AppConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_Tahap1_KPL_kelompok3.Configuration
+{
+    public class AppConfig
+    {
+        public int BatasPoinPeringatan { get; set; }
+        public int BatasPoinPanggilanOrangTua { get; set; }
+        public int BatasPoinSkorsing { get; set; }
+
+        public bool NotifikasiEmailAktif { get; set; }
+        public bool NotifikasiWhatsappAktif { get; set; }
+
+        public string DefaultRoleUserBaru { get; set; }
+
+        public AppConfig() { }
+
+        public AppConfig(int batasPoinPeringatan, int batasPoinPanggilanOrangTua, int batasPoinSkorsing, bool notifEmail, bool notifWa, string defaultRole)
+        {
+            BatasPoinPeringatan = batasPoinPeringatan;
+            BatasPoinPanggilanOrangTua = batasPoinPanggilanOrangTua;
+            BatasPoinSkorsing = batasPoinSkorsing;
+            NotifikasiEmailAktif = notifEmail;
+            NotifikasiWhatsappAktif = notifWa;
+            DefaultRoleUserBaru = defaultRole;
+        }
+    }
+}
