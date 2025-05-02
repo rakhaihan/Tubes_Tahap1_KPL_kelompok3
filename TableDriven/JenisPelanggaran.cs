e72c0048054a112ddfc0c3acb0caf0f0fd846a33
adiityanugraha <150656990+adiityanugraha@users.noreply.github.com>
2025-05-02 17:32:02 +0700

    Add files via upload
    Penambahan code reuse/library

---
+++ TableDriven/JenisPelanggaran.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoinSiswa.Library.TableDriven
+{
+    public class JenisPelanggaran
+    {
+        public string Nama { get; set; }
+        public LevelPelanggaran Level { get; set; }
+        public int Poin { get; set; }
+
+        public JenisPelanggaran(string nama, LevelPelanggaran level, int poin)
+        {
+            Nama = nama;
+            Level = level;
+            Poin = poin;
+        }
+    }
+}
---
+++ Utils/Formatter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoinSiswa.Library.Utils
+{
+    internal class Formatter
+    {
+        public static string FormatTanggal(DateTime tanggal) => tanggal.ToString("dd MMM yyyy");
+    }
+}
