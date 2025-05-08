6dcfe484ac2c7f2649b3c7852cc5bb842a526846
adiityanugraha <adityanugraha789@gmail.com>
2025-05-08 10:04:08 +0700

    Menambahkan Library

---
+++ PoinSiswa.Library/ComponentsLibrary/FormBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoinSiswa.Library.Components
+{
+    public static class FormBuilder
+    {
+        public static void TampilkanForm<T>(Dictionary<string, Func<T, string>> fieldGenerators, T data)
+        {
+            foreach (var field in fieldGenerators)
+            {
+                string label = field.Key;
+                string value = field.Value.Invoke(data);
+
+                Console.WriteLine($"{label}: {value}");
+            }
+        }
+    }
+}
+
---
+++ PoinSiswa.Library/ModelLibrary/Siswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoinSiswa.Library.Model
+{
+    public class Siswa
+    {
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public string Kelas { get; set; }
+        public int TotalPoin { get; set; }
+        public List<Pelanggaran> RiwayatPelanggaran { get; set; } = new();
+    }
+}
