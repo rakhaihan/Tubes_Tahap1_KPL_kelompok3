d8f0593b34a230000082c7ba5f5d7076545e8203
adiityanugraha <adityanugraha789@gmail.com>
2025-05-08 22:43:25 +0700

    Add PoinSiswa.Library module

---
+++ PoinSiswa.Library/AutomataLibrary/TriggerLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoinSiswa.Library.AutomataLibrary
+{
+    public enum Trigger
+    {
+        SETUJUI,
+        BERI_SANKSI,
+        SELESAIKAN
+    }
+}
---
+++ PoinSiswa.Library/ModelLibrary/PenggunaLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoinSiswa.Library.ModelLibrary
+{
+    public class Pengguna
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public RolePengguna Role { get; set; }
+    }
+}
