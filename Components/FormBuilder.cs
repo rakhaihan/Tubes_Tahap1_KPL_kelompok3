df71982c852793804283d8c56df25191775867f6
Ra.khaihan <rakharaihan2244@gmail.com>
2025-05-08 20:21:46 +0700

    Menambahkan FormBuilder dan TableRenderer sebelum merge

---
+++ Components/FormBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_Tahap1_KPL_kelompok3.Components
+{
+    public static class FormBuilder
+    {
+        public static void TampilkanForm<T>(Dictionary<string, Func<T, string>> fieldGenerators, T data)
+        {
+            foreach (var field in fieldGenerators)
+            {
+                string label = field.Key;
+                string value = field.Value.Invoke(data);
+
+                Console.WriteLine($"{label}: {value}");
+            }
+        }
+    }
+}
+
